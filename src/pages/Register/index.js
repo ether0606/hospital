9d64693a69e265f522190640b815302ddc3b75dc
Nurkhankazi <WDPF@nh.ctg>
2024-10-01 12:15:03 +0600

    update register

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,46 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AuthLayout from '../../layouts/AuthLayout';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { register } from '../../Api/AllApi';
 
 function Register() {
+    const [inputs, setInputs] = useState([]);
+    const navigate = useNavigate();
+    const handleChange = (event) => {
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({ ...values, [name]: value }));
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        await register(inputs);
+        //navigate('/login')
+    }
   return (
     <AuthLayout>
-       
         <div className="text-center mb-5">
             <img src="assets_admin/images/hospital-logo.png" height="48" className='mb-4'/>
             <h3>Sign Up</h3>
             <p>Please fill the form to register.</p>
         </div>
-        <form action="index.html">
+        <form onSubmit={handleSubmit}>
             <div className="row">
-                <div className="col-md-6 col-12">
+                <div className="col-md-12 col-12">
                     <div className="form-group">
-                        <label htmlFor="first-name">First Name</label>
-                        <input type="text" id="first-name" className="form-control"  name="first-name"/>
-                    </div>
-                </div>
-                <div className="col-md-6 col-12">
-                    <div className="form-group">
-                        <label htmlFor="last-name">Last Name</label>
-                        <input type="text" id="last-name" className="form-control"  name="last-name"/>
+                        <label htmlFor="first-name">Full Name</label>
+                        <input type="text" id="first-name" className="form-control" onChange={handleChange} name="name"/>
                     </div>
                 </div>
                 <div className="col-md-12 col-12">
                     <div className="form-group">
                         <label htmlFor="email">Email</label>
-                        <input type="email" id="email" className="form-control" name="email"/>
-                    </div>
-                </div>
-                <div className="col-md-12 col-12">
-                    <div className="form-group">
-                        <label htmlFor="contact-no">Contact Number</label>
-                        <input type="text" id="contact-no" className="form-control" name="contact-no"/>
+                        <input type="email" id="email" className="form-control" onChange={handleChange} name="email"/>
                     </div>
                 </div>
                 <div className="col-md-12 col-12">
                     <div className="form-group">
                         <label htmlFor="password">Password</label>
-                        <input type="text" id="password" className="form-control" name="password"/>
+                        <input type="text" id="password" className="form-control" onChange={handleChange} name="password"/>
+                    </div>
+                </div>
+                <div className="col-md-12 col-12">
+                    <div className="form-group">
+                        <label htmlFor="password">Confirm Password</label>
+                        <input type="text" id="password" className="form-control" onChange={handleChange} name="c_password"/>
                     </div>
                 </div>
                
